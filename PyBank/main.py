d190fb99a400b5052629a87ca748956816466b1c
Brody <wacker.brody@gmail.com>
2019-12-30 17:47:32 -0500

    Removing unsaved typos and copying contents of .ipynb to .py file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,3 @@
-# import libraries
-from pathlib import Path
-import csv
-
 # setting path for csv file
 csvpath = Path('../../../CU-NYC-FIN-PT-12-2019-U-C/Homework/02-Python/Instructions/PyBank/Resources/budget_data.csv')
 
@@ -9,6 +5,7 @@ csvpath = Path('../../../CU-NYC-FIN-PT-12-2019-U-C/Homework/02-Python/Instructio
 month_count = 0
 total_pnl = 0
 pnl_list = []
+date_list = []
 profit_increase = ''
 profit_decrease = ''
 
@@ -32,37 +29,57 @@ with open(csvpath, 'r') as csvfile:
         
         # creating list of pnl values
         pnl_list.append(int(pnl))
+        
+        #creating a list of dates
+        date_list.append(date)
     
     # creating variables for change values and a list to store these values
     pnl_change = 0
     pnl_change_list = []
     
-    # creating variables to reference where we are in the list - these will be increased as we iterate
-    # through the list of pnl values to change the inputs to the change value calculation
-    ref_value_1 = 0
-    ref_value_2 = 1
+    # setting first value of change value list to zero
+    pnl_change_list.append(0)
     
-    # settings variables for change value calculation
-    value_1 = pnl_list[ref_value_1]
-    value_2 = pnl_list[ref_value_2]
-    
-    # setting first value of change value list to the first value in the list - i.e. difference between
-    # first value and zero
-    pnl_change_list.append(value_1)
-    
-    # iterating through the list of pnl values
-    for value in pnl_list:
-            
-        # increase the list index number we are referencing in the list by 1
-        ref_value_1 += 1
-        ref_value_2 += 1
-            
-        # and assign the corresponding values to the change value calculation inputs
-        # value_1 = pnl_list[ref_value_1]
-        value_2 = pnl_list[ref_value_2]
-            
-        # then calculate the change and store this value in a variable
+    # iterating through the list of adjacent pnl values
+    for value_1, value_2 in zip(pnl_list[::1], pnl_list[1::1]):
+
+        # calculate the change and store this value in a variable
         pnl_change = value_2 - value_1
             
         # and add this variable to the change value list
         pnl_change_list.append(pnl_change)
+      
+    # sum changes for average
+    sum_of_changes = 0
+    
+    for value in pnl_change_list:
+        sum_of_changes += int(value)
+    
+    # calculating average change
+    average_change = float(sum_of_changes) / len(pnl_change_list)
+    
+    # creating a dictionary to associate the list of dates and list of pnl changes
+    dates_and_pnl = {date_list[i] : pnl_change_list[i] for i in range(len(date_list))}
+    
+    # setting variables to track largest profits and losses
+    max_profit = 0
+    max_loss = 0
+    
+    # iterating through the dictionary and return the largest profits and losses
+    for date, value in dates_and_pnl.items():
+        if value > max_profit:
+            max_profit = value
+        if value < max_loss:
+            max_loss = value
+            
+    # printing out summary statistics
+    print('Financial Analysis')
+    print('----------------------------')
+    print(f'Total Months: {month_count}')
+    print(f'Total: ${total_pnl}')
+    print(f'Average  Change: ${average_change}')
+    for date, value in dates_and_pnl.items():
+        if value == max_profit:
+            print(f'Greatest Increase in Profits: {date} | ${value}')
+        if value == max_loss:
+            print(f'Greatest Decrease in Profits: {date} | ${value}')
