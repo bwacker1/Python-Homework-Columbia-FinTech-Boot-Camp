cf548555ded33d3920d0c3b699d380e18b3bcd11
Brody Wacker <58641851+bwacker1@users.noreply.github.com>
2020-01-03 18:25:54 -0500

    Adding updates to export output to a text file

--- PyBank/main.py
+++ PyBank/main.py
@@ -26,16 +26,16 @@ with open(csvpath, 'r') as csvfile:
         # counting number of rows in the csv file and assigning this value to variable
         month_count += 1
         
-        #summing pnl values
+        # summing pnl values
         total_pnl += int(pnl)
         
-        # creating list of pnl values
+        # populating list of pnl values
         pnl_list.append(int(pnl))
         
-        #creating a list of dates
+        # populating list of dates
         date_list.append(date)
     
-    # creating variables for change values and a list to store these values
+    # creating variables for change values and a list to store these values once calculated
     pnl_change = 0
     pnl_change_list = []
     
@@ -45,13 +45,13 @@ with open(csvpath, 'r') as csvfile:
     # iterating through the list of adjacent pnl values
     for value_1, value_2 in zip(pnl_list[::1], pnl_list[1::1]):
 
-        # calculate the change and store this value in a variable
+        # calculating the change and assigning this value to a variable
         pnl_change = value_2 - value_1
             
-        # and add this variable to the change value list
+        # adding this variable to the change value list
         pnl_change_list.append(pnl_change)
       
-    # sum changes for average
+    # summing changes for average calculation
     sum_of_changes = 0
     
     for value in pnl_change_list:
@@ -59,7 +59,7 @@ with open(csvpath, 'r') as csvfile:
     
     # calculating average change
     average_change = sum_of_changes / len(pnl_change_list)
-    
+
     # creating a dictionary to associate the list of dates and list of pnl changes
     dates_and_pnl = {date_list[i] : pnl_change_list[i] for i in range(len(date_list))}
     
@@ -67,21 +67,37 @@ with open(csvpath, 'r') as csvfile:
     max_profit = 0
     max_loss = 0
     
-    # iterating through the dictionary and return the largest profits and losses
+    # iterating through the dictionary to determine the largest profits and losses
     for date, value in dates_and_pnl.items():
         if value > max_profit:
             max_profit = value
         if value < max_loss:
             max_loss = value
-            
-    # printing out summary statistics
-    print('Financial Analysis')
-    print('----------------------------')
-    print(f'Total Months: {month_count}')
-    print(f'Total: ${total_pnl}')
-    print(f'Average  Change: ${average_change}')
+    
+# setting output file name
+output = 'summary_stats.txt'
+
+# opening the output path as a file object and writing to file
+with open(output, 'w') as file:
+    file.write('Financial Analysis' + '\n'
+    '----------------------------' + '\n'
+    f'Total Months: {month_count}' + '\n'
+    f'Total: ${total_pnl}' + '\n'
+    f'Average  Change: ${average_change}' + '\n')
     for date, value in dates_and_pnl.items():
         if value == max_profit:
-            print(f'Greatest Increase in Profits: {date} | ${value}')
+            file.write(f'Greatest Increase in Profits: {date} (${value})' + '\n')
         if value == max_loss:
-            print(f'Greatest Decrease in Profits: {date} | ${value}')
+            file.write(f'Greatest Decrease in Profits: {date} (${value})')
+            
+# printing out summary statistics in line
+print('Financial Analysis' + '\n'
+'----------------------------' + '\n'
+f'Total Months: {month_count}' + '\n'
+f'Total: ${total_pnl}' + '\n'
+f'Average  Change: ${average_change}' + '\n')
+for date, value in dates_and_pnl.items():
+    if value == max_profit:
+        print(f'Greatest Increase in Profits: {date} (${value})' + '\n')
+    if value == max_loss:
+        print(f'Greatest Decrease in Profits: {date} (${value})')
